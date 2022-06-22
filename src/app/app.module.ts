46be628de730233648c61049cd47d093be89758f
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2022-06-22 12:22:16 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { StoreAppComponent } from './components/store-app/store-app.component';
+import { FiltersComponent } from './components/store-app/filters/filters.component';
+import { ProductListComponent } from './components/store-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    StoreAppComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
